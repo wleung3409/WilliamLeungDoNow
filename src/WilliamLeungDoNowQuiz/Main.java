e01ec3d4d74297ea227201e415ddd6d158a45874
wleung3409 <43207404+wleung3409@users.noreply.github.com>
2018-10-12 08:21:31 -0400

    Update Main.java

--- src/WilliamLeungDoNowQuiz/Main.java
+++ src/WilliamLeungDoNowQuiz/Main.java
@@ -8,12 +8,12 @@ public class Main {
     public static void main(String[]args){
 	// write your code here
         Scanner input= new Scanner(System.in);
-        System.out.println("What is your number?");
+        System.out.println("Enter an integer n");
         int number = input.nextInt();
         int sum = 0;
         for (int x = 0; x <= number; x++) {
             sum = sum + x;
         }
-        System.out.println("All the numbers that add up to yur number is "+sum);
+        System.out.println("The sum from 1 to n is "+sum);
     }
 }
